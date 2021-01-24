a6847250c8595d5eebcb5dd1d24e1926e5ecbc8d
KerolosAdib <51475429+KerolosAdib@users.noreply.github.com>
2021-01-24 14:35:34 -0800

    Add files via upload

---
+++ spawnpoints.js
@@ -0,0 +1,18 @@
+class spawnpoints
+{
+	constructor(game, x, y)
+	{
+		Object.assign(this, {game, x, y});
+		this.spritesheet = ASSET_MANAGER.getAsset(/*place spritesheet here*/);
+	};
+
+	update()
+	{
+
+	};
+
+	draw(ctx)
+	{
+		
+	};
+};
---
+++ textboxes.js
@@ -0,0 +1,18 @@
+class textboxes
+{
+	constructor(game, x, y)
+	{
+		Object.assign(this, {game, x, y});
+		this.spritesheet = ASSET_MANAGER.getAsset(/*place spritesheet here*/);
+	};
+
+	update()
+	{
+
+	};
+
+	draw(ctx)
+	{
+		
+	};
+};
