75f4e480bd44a884ff0a7c1d1875ee45c19d8740
Kero Adib <keroadib@live.com>
2021-01-24 18:36:44 -0800

    floors and walls update

--- floorsandwalls.js
+++ floorsandwalls.js
@@ -1,18 +1,55 @@
-class floorsandwalls
-{
-	constructor(game, x, y)
-	{
-		Object.assign(this, {game, x, y});
-		this.spritesheet = ASSET_MANAGER.getAsset(/*place spritesheet here*/);
+class floorsandwalls {
+	constructor(game, x, y) {
+		Object.assign(this, { game, x, y });
+		this.spritesheet = ASSET_MANAGER.getAsset("./sprites/floorsandwalls.png");
+		this.floor =
+			new animator(this.spritesheet, // Spritesheet
+				48, //X
+				0, //Y
+				16, //Width
+				16, //Height
+				1, //Frames
+				0.12, //Time
+				3, //Padding
+				false, //reverse
+				true, // looping,
+				null)
+		this.walls = [];
+		this.walls["left"] =
+			new animator(this.spritesheet,
+				16,
+				32,
+				16,
+				16,
+				1,
+				0.12,
+				12,
+				false,
+				true,
+				null)
+		this.walls["right"] =
+			new animator(this.spritesheet,
+				80,
+				32,
+				16,
+				16,
+				1,
+				0.12,
+				12,
+				false,
+				true,
+				null)
+
 	};
 
-	update()
-	{
+	update() {
 
 	};
 
 	draw(ctx)
 	{
-		
+		this.floor.drawFrame(this.game.clockTick, ctx, this.x+90, this.y, 1)
+		this.walls["left"].drawFrame(this.game.clockTick, ctx, this.x, this.y, 1)
+		this.walls["right"].drawFrame(this.game.clockTick, ctx, this.x+180, this.y, 1)
 	};
 };
--- main.js
+++ main.js
@@ -8,6 +8,7 @@ ASSET_MANAGER.queueDownload("./sprites/blade_2_fix.png")
 ASSET_MANAGER.queueDownload("./sprites/spike_strip.png")
 ASSET_MANAGER.queueDownload("./sprites/slimesprite/Slime_Walk.png")
 ASSET_MANAGER.queueDownload("./sprites/wolfsheet1.png")
+ASSET_MANAGER.queueDownload("./sprites/floorsandwalls.png")
 
 
 ASSET_MANAGER.downloadAll(function () {
@@ -20,6 +21,6 @@ ASSET_MANAGER.downloadAll(function () {
 	//gameEngine.addEntity(new Hoppers(gameEngine, 400, 600))
 	gameEngine.addEntity(new traps(gameEngine, 80, 80))
 	gameEngine.addEntity(new Chompers(gameEngine, 400, 500))
-
+	gameEngine.addEntity(new floorsandwalls(gameEngine, 300, 200))
 	gameEngine.start();
 });
