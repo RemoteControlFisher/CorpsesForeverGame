de61bb3626d347c33d4980ea171006d36a07cf06
Rory fisher <rochofi@gmail.com>
2021-01-30 18:46:34 -0800

    Parser for the level.

--- levels.js
+++ levels.js
@@ -3,5 +3,50 @@ var tutorialLevel = {
             x: 0,
             y: 0,
             type: "floor"
+      },
+      {
+            x: 1,
+            y: 0,
+            type: "floor"
+      },
+      {
+            x: 2,
+            y: 0,
+            type: "floor"
+      },
+      {
+            x: 3,
+            y: 0,
+            type: "floor"
+      },
+      {
+            x: 4,
+            y: 0,
+            type: "floor"
+      },
+      {
+            x: 5,
+            y: 0,
+            type: "floor"
+      },
+      {
+            x: 6,
+            y: 0,
+            type: "floor"
+      },
+      {
+            x: 7,
+            y: 0,
+            type: "floor"
+      },
+      {
+            x: 8,
+            y: 0,
+            type: "floor"
+      },
+      {
+            x: 9,
+            y: 0,
+            type: "floor"
       }]
 };
