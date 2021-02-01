552192b3e0b9855e33367f69f1ddee4ccbb4c236
Kero Adib <keroadib@live.com>
2021-01-31 20:42:37 -0800

    TextBoxes added

--- textboxes.js
+++ textboxes.js
@@ -3,7 +3,19 @@ class textboxes
 	constructor(game, x, y)
 	{
 		Object.assign(this, {game, x, y});
-		this.spritesheet = ASSET_MANAGER.getAsset(/*place spritesheet here*/);
+		this.spritesheet = ASSET_MANAGER.getAsset("./sprites/TempTextbox.png");
+		this.textbox =
+		new animator(this.spritesheet, // Spritesheet
+			275, //X
+			76, //Y
+			337, //Width
+			114, //Height
+			1, //Frames
+			0.12, //Time
+			3, //Padding
+			false, //reverse
+			true, // looping,
+			null)
 	};
 
 	update()
@@ -13,6 +25,6 @@ class textboxes
 
 	draw(ctx)
 	{
-		
+		this.textbox.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1)
 	};
 };
