8464e84cddf8ca91721024a310c08e47341f47f7
Rory fisher <rochofi@gmail.com>
2021-03-11 18:57:43 -0800

    Debug boxes off.

--- goal.js
+++ goal.js
@@ -66,7 +66,7 @@ class goal {
         this.fAnimation.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y - this.game.camera.y, 2);
         this.cAnimation.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x + 28, this.y - this.game.camera.y + 2, 2);
         this.bAnimation.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x + 64, this.y - this.game.camera.y, 2);
-        ctx.strokeStyle = 'Red';
-        ctx.strokeRect(this.BB.x - this.game.camera.x, this.BB.y - this.game.camera.y, this.BB.width, this.BB.height);
+        // ctx.strokeStyle = 'Red';
+        // ctx.strokeRect(this.BB.x - this.game.camera.x, this.BB.y - this.game.camera.y, this.BB.width, this.BB.height);
     };
 };
