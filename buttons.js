f32b89e961c9a9bbd4793f0b75905f18c9c33576
Rory fisher <rochofi@gmail.com>
2021-03-14 17:33:39 -0700

    Removed scary debug outline.

--- buttons.js
+++ buttons.js
@@ -98,8 +98,8 @@ class buttons
     draw(ctx) {
 
         this.button[this.state].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y - this.game.camera.y, .75)
-        ctx.strokeStyle = 'Red';
-        ctx.strokeRect(this.BB.x - this.game.camera.x, this.BB.y - this.game.camera.y, this.BB.width, this.BB.height);
+        //ctx.strokeStyle = 'Red';
+        //ctx.strokeRect(this.BB.x - this.game.camera.x, this.BB.y - this.game.camera.y, this.BB.width, this.BB.height);
     };
 
     // corpseHelper() {
